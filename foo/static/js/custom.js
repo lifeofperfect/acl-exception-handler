32c4fb8f124a3489d17b38e558a6300eb1f7d3ce
lifeofatom <perfect.igbadumhe@gmail.com>
2020-04-15 19:45:39 +0100

    'done'

--- foo/static/js/custom.js
+++ foo/static/js/custom.js
@@ -1,7 +1,17 @@
-console.log('again again!!!')
-console.log('again again!!!')
-console.log('again again!!!')
-console.log('again again!!!')
-console.log('again again!!!')
-console.log('again again!!!')
-console.log('again again!!!')
+$(document).ready(function() {
+    $("#id_Date_Discovered").datepicker({changeYear: true, changeMonth:true, dateFormat: 'yy-mm-dd'});
+    $("#id_AC_OPEN_DATE").datepicker({changeYear: true, changeMonth:true, dateFormat: 'yy-mm-dd'});
+    $("#id_ONBOARDING_DATE").datepicker({changeYear: true, changeMonth:true, dateFormat: 'yy-mm-dd'});
+})
+
+document.getElementById('id_Loss_prevented_LCY').addEventListener('input', function(e){
+    let naira = e.target.value;
+    let pp =document.getElementById('id_Loss_prevented_USD').value
+    var rate = document.getElementById('id_LCY_USD_RATE').value;
+    
+    var convert = parseFloat(naira) / parseFloat(rate)
+    
+    document.getElementById('id_Loss_prevented_USD').value = convert.toFixed(3)
+    document.getElementById('id_Amount_Involved_LCY').value = naira
+  })
+
